934d14686a9c735bfe63addb7e6933a409d58bf4
y-sadayuki <chada0913@gmail.com>
2023-08-11 11:22:08 +0900

    QuetyrとMutationが動くところまで作成

---
+++ appsync-lambda/src/getItem.py
@@ -0,0 +1,23 @@
+import json
+import os
+import random
+from datetime import datetime, timedelta
+
+def lambda_handler(event, context):
+
+    print("event=", event)  
+    print("context=", context)  
+
+    # クエリの引数から ID を取得
+    item_id = event["id"]
+
+    # ID に基づいてデータを取得（この部分は適切な処理を行う必要があります）
+    response = {
+        "id": "1234",
+        "hogetext": "Sample Item",
+    }
+
+    print(response)
+
+    return response
+
