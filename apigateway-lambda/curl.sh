ba9b5d483a7f2cedc843bb5dc5c433a8a82df0a3
y-sadayuki <chada0913@gmail.com>
2023-08-13 14:41:13 +0900

    sam+apikayの構成でデプロイする三プロを更新

--- apigateway-lambda/curl.sh
+++ apigateway-lambda/curl.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-API_KEY="Bvap13tJpc9GrQshKEHO24Y3wiK9CXPu3xdO9jWb"
-API_ENDPOINT="https://v1smjjgayh.execute-api.ap-northeast-1.amazonaws.com/Prod/get"
-
+# API_KEY is the API key generated in the API Gateway console
+API_KEY="xxxxxxxxxxxxxxxxxxxxxxxxxx"
+API_ENDPOINT="https://xxxxxxxxxxxxxxxxxxxxx.com/Prod/get"
 curl -X GET "$API_ENDPOINT" -H "x-api-key: $API_KEY"
-#curl -X GET "$API_ENDPOINT" 
